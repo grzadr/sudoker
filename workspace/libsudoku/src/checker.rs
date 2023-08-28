49e9dd5e1c400ea451dc7461e715082a46c509ff
Adrian Grzemski <adrian.grzemski@gmail.com>
2023-08-28 20:09:22 +0200

    Added tests

--- workspace/libsudoku/src/checker.rs
+++ workspace/libsudoku/src/checker.rs
@@ -35,10 +35,60 @@ impl ValChecker {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_valchecker_new() {
-        let temp = ValChecker::new(3);
-        assert_eq!(temp.values, vec![[false; 3]; 3])
+    const TOP: u8 = 4;
+
+    fn gen_values(top: u8) -> Vec<(u8, u8)> {
+        let top_range = 1..=top;
+        top_range
+            .clone()
+            .flat_map(|x| top_range.clone().map(move |y| (x, y)))
+            .collect()
     }
 
+    #[test]
+    fn test_new() {
+        let temp = ValChecker::new(TOP);
+        assert_eq!(temp.values, vec![vec![false; TOP as usize]; TOP as usize]);
+    }
+
+    #[test]
+    fn test_set() {
+        let mut temp = ValChecker::new(TOP);
+        let values = gen_values(TOP);
+
+        for (ele, val) in values {
+            temp.set(ele, val);
+        }
+
+        assert_eq!(temp.values, vec![vec![true; TOP as usize]; TOP as usize]);
+    }
+
+    #[test]
+    fn test_unset() {
+        let mut temp = ValChecker::new(TOP);
+        let values = gen_values(TOP);
+
+        for (ele, val) in values.clone() {
+            temp.set(ele, val);
+        }
+
+        for (ele, val) in values {
+            temp.unset(ele, val);
+        }
+
+        assert_eq!(temp.values, vec![vec![false; TOP as usize]; TOP as usize]);
+    }
+
+    #[test]
+    fn test_is_solved() {
+        let mut temp = ValChecker::new(TOP);
+        let values = gen_values(TOP);
+
+        for (ele, val) in values {
+            assert_ne!(temp.is_solved(), true);
+            temp.set(ele, val);
+        }
+
+        assert_eq!(temp.is_solved(), true);
+    }
 }
